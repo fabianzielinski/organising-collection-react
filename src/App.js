5f72244ee890588f9ff223d2821d6df3b2805ee5
fabianzielinski <fabian.zielinski@o2.pl>
2022-01-17 21:11:10 +0100

    Basic usability

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import XLSX from "xlsx";
+import "./App.css";
+import TableDisplay from "./TableDisplay";
+import DataInput from "./DataInput";
+import DragDropFile from "./DragDropFile";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [cols, setCols] = useState([]);
+
+  const handleFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const ab = e.target.result;
+      const wb = XLSX.read(ab, { type: "array" });
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      setData(data);
+      setCols(make_cols(ws["!ref"]));
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  const exportFile = () => {
+    const ws = XLSX.utils.aoa_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
+
+    XLSX.writeFile(wb, "sheetjs.xlsx");
+  };
+
+  const make_cols = (refstr) => {
+    let o = [],
+      C = XLSX.utils.decode_range(refstr).e.c + 1;
+    for (var i = 0; i < C; ++i)
+      o[i] = { name: XLSX.utils.encode_col(i), key: i };
+    return o;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropFile handleFile={handleFile}>
+      <div className="row">
+        <div className="col-xs-12">
+          <DataInput handleFile={handleFile} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <button
+            disabled={!data.length}
+            className="btn btn-success"
+            onClick={exportFile}
+          >
+            Export
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-12">
+          <TableDisplay data={data} cols={cols} />
+        </div>
+      </div>
+    </DragDropFile>
   );
 }
 
