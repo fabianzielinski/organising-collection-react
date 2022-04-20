c1c32de73edcfed2255dfbd8860be6a8891b81d2
fabianzielinski <fabian.zielinski@o2.pl>
2022-04-20 23:57:10 +0200

    Add react-router-dom

---
+++ client/src/components/features/FormSearch.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FormSearch = (props) => {
+  return (
+    <form action="">
+      <label htmlFor="serch">
+        Szukaj :
+        <input
+          name="search"
+          type="text"
+          id="search"
+          onChange={props.change}
+          value={props.serchText}
+        />
+      </label>
+    </form>
+  );
+};
+
+export default FormSearch;
